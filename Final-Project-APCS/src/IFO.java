4514f9400d352bc0154189aae95386eaff76bf89
prateeknarayanan@gmail.com <prateeknarayanan@gmail.com>
2017-05-13 11:14:09 -0700

    Started IFO class

--- Final-Project-APCS/src/IFO.java
+++ Final-Project-APCS/src/IFO.java
@@ -1,4 +1,13 @@
 
 public class IFO {
-
+	private Location myLoc;
+	private Color myColor;
+	private final int SPEED;
+	public IFO(Location loc, Color c, int speed)
+	{
+		myLoc = loc;
+		myColor =c;
+		SPEED = speed;
+		
+	}
 }
