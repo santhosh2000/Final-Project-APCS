49863bc83891d8312fe870a906d90670ad1058a6
prateeknarayanan@gmail.com <prateeknarayanan@gmail.com>
2017-05-12 18:18:36 -0700

    Added more base classes and worked on Location class.

--- Final-Project-APCS/src/Actor.java
+++ Final-Project-APCS/src/Actor.java
@@ -2,6 +2,12 @@ import java.awt.image.*;
 
 public class Actor
 {
-	private (Location) loc;
+	private Location loc;
 	private Image img;
+	public Actor(Location l, Image i)
+	{
+		loc = l;
+		img = i;
+	}
+	
 }
---
+++ Final-Project-APCS/src/Location.java
@@ -0,0 +1,34 @@
+
+public class Location {
+	private int myWidth;
+	private int myHeight;
+public Location(int width, int height)
+	{
+		myWidth=width;
+		myHeight=height;
+	}
+public void setWidth(int width)
+{
+	myWidth = width;
+	}
+public int getWidth()
+{
+	return myWidth;
+	}
+public void setHeight(int height)
+{
+	myHeight = height;
+	}
+public int getHeight()
+{
+	return myHeight;
+	}
+public void setLocation(int width, int height)
+{
+	
+	}
+public Location getLocation()
+{
+	return new Location(getWidth(), getHeight());
+	}
+}
