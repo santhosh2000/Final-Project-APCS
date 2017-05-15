8c2b3658f0828be14ee3876cc8d1b083185163de
prateeknarayanan@gmail.com <prateeknarayanan@gmail.com>
2017-05-15 10:11:50 -0700

    Moveable interface is complete. New methods added to Actor class.

--- Final-Project-APCS/src/Actor.java
+++ Final-Project-APCS/src/Actor.java
@@ -4,10 +4,17 @@ public class Actor
 {
 	private Location loc;
 	private Image img;
+	private final int STEP_SIZE;
 	public Actor(Location l, Image i)
 	{
 		loc = l;
 		img = i;
 	}
+	public void setTarget(){}
+	public void moveLeft(){}
+	public void moveRight(){}
+	public void shoot(){}
+	public void explode(){}
+	public boolean contains(Location point){}
 	
 }
--- Final-Project-APCS/src/Moveable.java
+++ Final-Project-APCS/src/Moveable.java
@@ -1,4 +1,4 @@
 
 public interface Moveable {
-
+	public void move();
 }
