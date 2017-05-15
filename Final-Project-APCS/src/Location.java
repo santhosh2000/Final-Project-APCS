c9a228b5ae1b7224e85fcb12cc60edb3777fcd30
justinngo99 <justinngo99@gmail.com>
2017-05-15 10:12:27 -0700

    Update Location.java

--- Final-Project-APCS/src/Location.java
+++ Final-Project-APCS/src/Location.java
@@ -1,34 +1,36 @@
 
-public class Location {
+public class Location
+{
 	private int myWidth;
 	private int myHeight;
-public Location(int width, int height)
+	public Location(int width, int height)
 	{
 		myWidth=width;
 		myHeight=height;
 	}
-public void setWidth(int width)
-{
-	myWidth = width;
+	public void setWidth(int width)
+	{
+		myWidth = width;
 	}
-public int getWidth()
-{
-	return myWidth;
+	public int getWidth()
+	{
+		return myWidth;
 	}
-public void setHeight(int height)
-{
-	myHeight = height;
+	public void setHeight(int height)
+	{
+		myHeight = height;
 	}
-public int getHeight()
-{
-	return myHeight;
+	public int getHeight()
+	{
+		return myHeight;
 	}
-public void setLocation(int width, int height)
-{
-	
+	public void setLocation(int width, int height)
+	{
+		myWidth = width;
+		myHeight = height;
 	}
-public Location getLocation()
-{
-	return new Location(getWidth(), getHeight());
+	public Location getLocation()
+	{
+		return new Location(getWidth(), getHeight());
 	}
 }
