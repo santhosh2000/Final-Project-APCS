cb8cd26aec89eb9da6b37a81e607b6c75d8d3143
prateeknarayanan@gmail.com <prateeknarayanan@gmail.com>
2017-05-15 11:00:39 -0700

    Deleted run method

--- Final-Project-APCS/src/EnemyBullet.java
+++ Final-Project-APCS/src/EnemyBullet.java
@@ -2,23 +2,16 @@
 public class EnemyBullet extends IFO implements Moveable
 {
 	private final int SPEED;
-	private final Location xLoc;
-	private final Location yLoc;
-	private final Location height;
-	public EnemyBullet(Color color, Location l, DrawingCanvas canvas, Location x, Location y, Location height)
+	private  Location loc;
+	private int height;
+	private Color color;
+	public EnemyBullet(Location l, int h,
+			DrawingCanvas canvas, Actor aShip, Color color)
 	{
-		super(c, loc);
 		
 	}
 	public void move()
 	{
 	}
-public Projectile(double x, double y, double height,
-DrawingCanvas canvas, SpaceShip aShip)
-Create the missile line at the given location with the given height, and set up instance
-variables. Call start().
-Methods:
-● public void run()
-Move the line down the screen until it goes off the bottom or hits the ship
-Then remove line from screen, and make the ship explode if appropriate.
+	
 }
