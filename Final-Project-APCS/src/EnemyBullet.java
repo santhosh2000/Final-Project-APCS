555b69b55c6b5909313011669fa5dd7fa98db22a
prateeknarayanan@gmail.com <prateeknarayanan@gmail.com>
2017-05-16 11:40:07 -0700

    Added time constant to IFO and bullet classes

--- Final-Project-APCS/src/EnemyBullet.java
+++ Final-Project-APCS/src/EnemyBullet.java
@@ -2,10 +2,10 @@
 public class EnemyBullet extends IFO implements Moveable
 {
 	private int height;
-	public EnemyBullet(Location l, int h,int speed,
+	public EnemyBullet(Location l, int h,int speed,int time,
 			DrawingCanvas canvas, Actor aShip, Color color)
 	{
-		super(l, color, speed);
+		super(l, color, speed, time);
 	}
 	public void run()
 	{
--- Final-Project-APCS/src/PlayerBullet.java
+++ Final-Project-APCS/src/PlayerBullet.java
@@ -2,9 +2,9 @@
 public class PlayerBullet extends IFO implements Moveable
 {
 
-	public PlayerBullet(Color color, Location l, int speed)
+	public PlayerBullet(Color color, Location l, int speed, int time)
 	{
-		super(color, l, speed);
+		super(color, l, speed, time);
 	}
 	public void run(){}
 	
