bd2d9d2fc3ebf5353c3cbfad96bba000c71d439b
prateeknarayanan@gmail.com <prateeknarayanan@gmail.com>
2017-05-16 11:50:24 -0700

    Added time parameter to Asteroid class

--- Final-Project-APCS/src/Asteroid.java
+++ Final-Project-APCS/src/Asteroid.java
@@ -1,11 +1,8 @@
 
 public class Asteroid extends IFO implements Moveable{
-	public final Color color;
-	public Location loc;
-	public final int SPEED;
-	public Asteroid(Color c, Location l, int s )
+	public Asteroid(Color c, Location l, int s, int t )
 	{
-		super(c, l, s);
+		super(c, l, s, t);
 	}
 	public void move()
 	{
