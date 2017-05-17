5ba4c1c3f3985c73bf970fe05c98201920547ab2
Santhosh <santho1999@gmail.com>
2017-05-16 19:38:43 -0700

    updated Player class

--- Final-Project-APCS/src/Player.java
+++ Final-Project-APCS/src/Player.java
@@ -1,4 +1,51 @@
 
 public class Player {
+	priavte boolean isMoving;
+	private boolean isAlive;
+	private Location myLoc;
+	private int myScore;
+
+	public Player(boolean a, boolean b, Location l)
+	{
+		isMoving = a;
+		isAlive = b;
+		myLoc = l;
+		myScore = 0;
+	}
+
+	public void setLoc(Location L)
+	{
+		myLoc = L;
+	}
+	public void setAlive(boolean a)
+	{
+		isAlive = a;
+	}
+	public void setMoving(boolean a)
+	{
+		isMoving = a;
+	}
+	public void setScore(int a)
+	{
+		myScore = a;
+	}
+
+
+	public Location getLoc()
+	{
+		return myLoc;
+	}
+	public boolean getMoving()
+	{
+		return isMoving;
+	}
+	public boolean getAlive()
+	{
+		return isAlive;	
+	}
+	public int getScore()
+	{
+		return myScore;
+	}
 
 }
