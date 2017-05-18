db5c556e6930ae2c8a542aeb6ea11626eb21d910
1906663 <1906663@00-46195.pusd.dom>
2017-05-18 11:40:25 -0700

    edits to the Enemy class

--- Final-Project-APCS/src/Enemy.java
+++ Final-Project-APCS/src/Enemy.java
@@ -1,4 +1,45 @@
-
-public class Enemy {
-
+import java.awt.image.*;
+public class Enemy implements Actor
+{
+	private int enemyCounter = 0;
+	private boolean isAlive;
+	private final int enemyShootTime;
+	private Image img;
+	private Location loc;
+	public Enemy(Image pic, Location l)
+	{
+		img = pic;
+		loc = l;
+	}
+	public void move(int dx)
+	{
+		loc.setWidth(loc.getWidth()+dx);
+	}
+	public boolean isAlive()
+	{
+		return isAlive;
+	}
+	public void explode()
+	{
+		
+	}
+	public void fire()
+	{
+		
+	}
+	public boolean willFire()
+	{
+		if(isAlive)
+			fire();
+	}
+	public void end()
+	{
+		
+	}
+	public void addHit()
+	{
+		if(hitEnemy())
+			enemyCounter++;
+	}
 }
+
